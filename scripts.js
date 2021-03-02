7fe40807aba9be0363d04c840a402f62a18615d5
Phil Flores <phil.flrs@gmail.com>
2021-03-02 15:12:55 -0700

    1.2 js basics part 1 completed

--- scripts.js
+++ scripts.js
@@ -1,1 +1,6 @@
-let favoriteFood = 'Pastaaah';
+let pokemonList = [
+    { name: 'Bulbasaur', height:7, type: ['grass', 'poison'] },
+    { name: 'Charmander', height:10,type: 'fire'},
+    { name: 'Articuno', height:15, type: ['ice', 'flying'] }
+];
+console.log(pokemonList);
