a2060de8c799f0d5314ccd9dd16ba2b2cd8f6f5d
Phil Flores <phil.flrs@gmail.com>
2021-03-15 10:04:35 -0600

    update scripts.js for 1.7
    added promises to get and load the details of pokemon from apiUrl
    renamed variable pokedex to pokemonList emptied array and adjusted other functions
    showDetails is called on button click

--- scripts.js
+++ scripts.js
@@ -1,62 +1,93 @@
 let pokemonRepo = (function() {
-    let pokedex = [ //funciton scope
-        { name: 'Bulbasaur', height: 4, weight: 12, types: ['grass', 'poison'] },
-        { name: 'Squirtle', height: 4, weight: 16, types: ['water'] },
-        { name: 'Charmander', height: 11, weight: 100, types: ['Fire'] }
-    ];
-
-    function add(pokemon) {
+    let pokemonList = [];
+    let apiUrl ='https://pokeapi.co/api/v2/pokemon/?limit=150';
+    
+    function add(pokemon) { //validation function
         if (
             typeof pokemon === "object" &&
-            "name" in pokemon &&
+            "name" in pokemon /*&&
+            "detailsUrl" in pokemon
             "height" in pokemon &&
-            "types" in pokemon
+            "types" in pokemon */
         ) {
-            pokedex.push(pokemon);
+            pokemonList.push(pokemon);
         } else {
             console.log("pokemon is not correct");
         }
     }
 
-    function getAll() {
-        return pokedex
+    function getAll() { //gets the pokemon
+        return pokemonList
     }
 
-    function showDetails(pokemon){
-        console.log(pokemon);
-    }
-
-    function addListItem(pokemon){
+    function addListItem(pokemon){ // function for dom manipulation to list pokemon as button
         let listOfPokemon = document.querySelector(".pokemon-list");
         let listedPokemon = document.createElement("li");
         let button = document.createElement("button");
         button.innerText = pokemon.name; // the pokemon will appear as a button
         button.classList.add("button-class");
         button.addEventListener('click', function (event){ // event here the function passed as the second param for the event listener function when it is clicked (the first param of the event listener function)
-            showDetails(pokemon);
+            showDetails(pokemon)
         });
         listedPokemon.appendChild(button);
         listOfPokemon.appendChild(listedPokemon);
     
     }
+    //ajax functions begin
+    //fuction to get the details of the pokemon uses a promise to fetch that data from api
+    function loadDetails(item){
+        let url = item.detailsUrl;
+        return fetch(url).then(function(response){
+            return response.json();
+        }).then(function(details){
+            //now add the details to the item
+            item.imageUrl = details.sprites.front_default;
+            item.height = details.height;
+            item.types =  details.types;
+        }).catch (function(e){
+            console.error(e);
+        });
+    }
+    //fuction to load the details for each pokemon
+    function loadList(){ //function to contact apiUrl defined above
+        return fetch(apiUrl).then(function (response){
+            return response.json();
+        }).then(function(json){
+            json.results.forEach(function (item){
+                let pokemon = {
+                    name: item.name,
+                    detailsUrl: item.url
+                };
+                add(pokemon);
+                console.log(pokemon);
+            });    
+        }).catch (function(e){
+            console.log(e);
+        })
+    }
+    //function to log the details of the pokemon
+    function showDetails(pokemon){ //gets called when a pokemon is clicked
+        loadDetails(pokemon).then(function(){ //is a promise to console log pokemon details of img url, height and types
+            console.log(pokemon);
+        });   
+    }
+    //ajax functions end
+    
     return { //returns an object of functions
         add: add,
         getAll: getAll,
-        addListItem: addListItem
-        //showDetails: showDetails
-    };
-})(); //calls the IIFE
-pokemonRepo.add({ name: 'Pikachu', height: 5, types: ['electric'] });
-pokemonRepo.add({ name: 'Raichu', height: 5, types: ['electric'] });
+        addListItem: addListItem,
+        loadList:loadList,
+        loadDetails:loadDetails,
+        showDetails: showDetails
+    }
+
+})();
 console.log(pokemonRepo.getAll());
 
 
-
-pokemonRepo.getAll().forEach(function(pokemon) { // the forEach method takes each pokemon from the array of pokemonRepo   
-    pokemonRepo.addListItem(pokemon); //uses the addListItem function to render each pokemon as a list item
-}); 
-
-// let button = document.querySelector("button");
-// button.addEventListener('click', function (event){ // event here the function passed as the second param for the event listener function when it is clicked (the first param of the event listener function)
-//     console.log(event);
-// });
+pokemonRepo.loadList().then(function() {
+    pokemonRepo.getAll().forEach(function(pokemon) { // the forEach method takes each pokemon from the pokemonRepo   
+        pokemonRepo.addListItem(pokemon); //uses the addListItem function to render each pokemon as a list item
+    }); 
+});
